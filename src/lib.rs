a4030a7aeb119b481a0ec40f784067ba83c55dca
Mason Fischer <mason@kissr.co>
2018-07-11 18:33:09 -0400

    Update wasm-rpc

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,9 @@
-#![feature(alloc)]
-#![no_std]
-extern crate alloc;
 extern crate wasm_rpc;
-use alloc::String;
-use alloc::vec::Vec;
 use wasm_rpc::*;
 
 extern {
-    fn sender() -> *const u8;
+    fn _sender() -> *const u8;
+    fn _block_hash() -> *const u8;
     fn read(key: *const u8) -> *const u8;
     fn _call(code: *const u8, method: *const u8, params: *const u8, storage_context: *const u8) -> *const u8;
     fn write(key: *const u8, value: *const u8);
@@ -16,6 +12,12 @@ extern {
 pub struct ElipitcoinBlockchain {}
 
 impl BlockChain for ElipitcoinBlockchain {
+    fn block_hash(&self) -> Vec<u8> {
+        unsafe {
+            _block_hash().as_raw_bytes()
+        }
+    }
+
     fn read(&self, key: Vec<u8>) -> Vec<u8> {
       unsafe {
         read(key.as_pointer()).as_raw_bytes()
@@ -23,8 +25,8 @@ impl BlockChain for ElipitcoinBlockchain {
     }
 
     fn sender(&self) -> Vec<u8> {
-        unsafe{
-            sender().as_raw_bytes()
+        unsafe {
+            _sender().as_raw_bytes()
         }
     }
 
@@ -52,6 +54,7 @@ impl BlockChain for ElipitcoinBlockchain {
 pub trait BlockChain {
     fn read(&self, key: Vec<u8>) -> Vec<u8>;
     fn write(&self, key: Vec<u8>, value: Vec<u8>);
+    fn block_hash(&self) -> Vec<u8>;
     fn sender(&self) -> Vec<u8>;
     fn call(&self, code: Vec<u8>, method: String, params: Vec<u8>, storage_context: Vec<u8>) -> Vec<u8>;
 
@@ -64,6 +67,7 @@ pub trait BlockChain {
     }
 
     fn write_u64(&self, key: Vec<u8>, value: u64) {
+        FromBytes::from_u64(value);
         self.write(key, FromBytes::from_u64(value));
     }
 }
